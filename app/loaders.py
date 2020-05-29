ba809b21dbe1e4a4b9488d99dc2cf80e3e466085
Handerson Contreras <handerson.contreras@gmail.com>
2020-05-29 09:33:04 -0600

    Update loaders.py

--- app/loaders.py
+++ app/loaders.py
@@ -5,7 +5,7 @@ from app.session.user import User
 @login_manager.user_loader
 def load_user(user_id):
     obj_user = UserModel.query.filter_by(email=user_id).first()
-    if obj_user:
-        return User(obj_user.email, obj_user.email) 
-    else:
-        None
+    if obj_user is None:
+        return None
+    
+    return User(obj_user.email, obj_user.email) 
